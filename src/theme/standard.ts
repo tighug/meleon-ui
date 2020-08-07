4ad95b821f56360df29ec414db87ede3026bcd8f
tighug <kapuriko123@gmail.com>
2020-08-07 12:51:40 +0900

    feat: Add standard color theme

---
+++ src/theme/standard.ts
@@ -0,0 +1,31 @@
+import palette from "meleon-palette";
+
+export default {
+  color: {
+    primary: {
+      light: palette.green.lighten2,
+      base: palette.green.base,
+      dark: palette.green.darken2,
+    },
+    secondary: {
+      light: palette.deepOrange.lighten2,
+      base: palette.deepOrange.base,
+      dark: palette.deepOrange.darken2,
+    },
+    background: {
+      primary: palette.lightText.primary,
+      secondary: palette.lightText.secondary,
+      disabled: palette.lightText.disabled,
+    },
+    text: {
+      primary: palette.darkText.primary,
+      secondary: palette.darkText.secondary,
+      disabled: palette.darkText.disabled,
+      dividers: palette.darkText.dividers,
+    },
+    icon: {
+      active: palette.darkIcons.active,
+      inactive: palette.darkIcons.inactive,
+    },
+  },
+};
