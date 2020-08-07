25dffa2d9c5cc0e18e50e3f7f78da383e307e2cd
tighug <kapuriko123@gmail.com>
2020-08-08 05:33:36 +0900

    chore: Add ress

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import React from "react";
 import { configure, addParameters, addDecorator } from "@storybook/react";
 import { themes } from "@storybook/theming";
 import themeDecorator from "./themeDecorator";
+import "ress";
 
 addDecorator(themeDecorator);
 addParameters({
