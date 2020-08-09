5e427698534685ec2bc0ecfbf0c3c6f1f77604e8
tighug <kapuriko123@gmail.com>
2020-08-09 13:16:11 +0900

    feat: Add system/spacing.ts

---
+++ src/components/system/spacing.ts
@@ -0,0 +1,45 @@
+import { css } from "styled-components";
+import space, { Size } from "../../utils/space";
+
+export type MarginProps = {
+  ma?: Size;
+  mx?: Size;
+  my?: Size;
+  mt?: Size;
+  mr?: Size;
+  mb?: Size;
+  ml?: Size;
+};
+
+export type PaddingProps = {
+  pa?: Size;
+  px?: Size;
+  py?: Size;
+  pt?: Size;
+  pr?: Size;
+  pb?: Size;
+  pl?: Size;
+};
+
+export type SpacingProps = MarginProps & PaddingProps;
+
+export const margin = css<MarginProps>`
+  margin: ${(props) => space(props.ma)};
+  margin-top: ${(props) => space(props.mt || props.my)};
+  margin-right: ${(props) => space(props.mr || props.mx)};
+  margin-bottom: ${(props) => space(props.mb || props.my)};
+  margin-left: ${(props) => space(props.ml || props.mx)};
+`;
+
+export const padding = css<PaddingProps>`
+  padding: ${(props) => space(props.pa)};
+  padding-top: ${(props) => space(props.pt || props.py)};
+  padding-right: ${(props) => space(props.pr || props.px)};
+  padding-bottom: ${(props) => space(props.pb || props.py)};
+  padding-left: ${(props) => space(props.pl || props.px)};
+`;
+
+export const spacing = css<SpaceProps>`
+  ${margin}
+  ${padding}
+`;
