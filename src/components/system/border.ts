9242d3365f9195029962e40af85a39ef4f428e8c
tighug <kapuriko123@gmail.com>
2020-08-09 13:14:07 +0900

    feat: Add system/border.ts

---
+++ src/components/system/border.ts
@@ -0,0 +1,25 @@
+import { css } from "styled-components";
+
+export type BorderProps = {
+  border?: number;
+  borderTop?: number;
+  borderRight?: number;
+  borderBottom?: number;
+  borderLeft?: number;
+
+  borderColor?: string;
+  borderStyle?: string;
+  borderRadius?: string;
+};
+
+export const border = css<BorderProps>`
+  border-color: ${(props) =>
+    props.borderColor || props.theme.color.bg.disabled};
+  border-style: ${(props) => props.borderStyle};
+  border-width: ${(props) => props.border}px;
+  border-top-width: ${(props) => props.borderTop}px;
+  border-right-width: ${(props) => props.borderRight}px;
+  border-bottom-width: ${(props) => props.borderBottom}px;
+  border-left-width: ${(props) => props.borderLeft}px;
+  border-radius: ${(props) => props.borderRadius};
+`;
