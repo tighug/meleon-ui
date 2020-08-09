c960d31a22469f24fa7f1919b24a8627a5f13637
tighug <kapuriko123@gmail.com>
2020-08-09 15:23:31 +0900

    feat: Update system/sizing.ts

--- src/components/system/sizing.ts
+++ src/components/system/sizing.ts
@@ -1,21 +1,30 @@
 import { css } from "styled-components";
 
 export type SizingProps = {
-  width?: string;
-  maxWidth?: string;
-  minWidth?: string;
-  height?: string;
-  maxHeight?: string;
-  minHeight?: string;
   boxSizing?: "content-box" | "border-box";
+  width?: string | number;
+  maxWidth?: string | number;
+  minWidth?: string | number;
+  height?: string | number;
+  maxHeight?: string | number;
+  minHeight?: string | number;
 };
+
+export function size(val: string | number = "auto"): string {
+  if (typeof val === "string") return val;
+
+  if (val < 0) return "auto";
+  if (val <= 1) return `${val * 100}%`;
+
+  return `${val}px`;
+}
 
 export const sizing = css<SizingProps>`
   box-sizing: ${(props) => props.boxSizing};
-  width: ${(props) => props.width};
-  min-width: ${(props) => props.minWidth};
-  max-width: ${(props) => props.maxWidth};
-  height: ${(props) => props.height};
-  min-height: ${(props) => props.minHeight};
-  max-height: ${(props) => props.maxHeight};
+  width: ${(props) => size(props.width)};
+  min-width: ${(props) => size(props.minWidth)};
+  max-width: ${(props) => size(props.maxWidth)};
+  height: ${(props) => size(props.height)};
+  min-height: ${(props) => size(props.minHeight)};
+  max-height: ${(props) => size(props.maxHeight)};
 `;
