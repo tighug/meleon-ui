3df599c86e9b799aa0afa1d66adec3d66b85c363
tighug <kapuriko123@gmail.com>
2020-08-21 20:22:32 +0900

    chore: Update .storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
+import { themes } from "@storybook/theming";
 import { addDecorator } from "@storybook/react";
 import themeDecorator from "./themeDecorator";
-import { themes } from "@storybook/theming";
 import "normalize.css";
 import "./index.css";
 import "@mdi/font/css/materialdesignicons.css";
@@ -8,6 +8,7 @@ import "@mdi/font/css/materialdesignicons.css";
 addDecorator(themeDecorator);
 
 export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
   docs: {
     theme: themes.dark,
   },
