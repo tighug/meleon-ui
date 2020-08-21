4d96a41825fd56be99800a9728516723eb0d809e
tighug <kapuriko123@gmail.com>
2020-08-21 11:53:42 +0900

    feat: Update standard.ts

--- src/themes/standard.ts
+++ src/themes/standard.ts
@@ -1,26 +1,23 @@
 import * as palette from "meleon-palette";
 
 export default {
-  colors: {
-    primary: palette.green.base,
-    secondary: palette.deepOrange.base,
-    normal: "#272727",
-    bg: {
-      primary: palette.grey.darken4,
-      secondary: palette.grey.darken3,
-      disabled: palette.grey.darken2,
-    },
-    text: {
-      primary: palette.lightText.primary,
-      secondary: palette.lightText.secondary,
-      disabled: palette.lightText.disabled,
-      dividers: palette.lightText.dividers,
-    },
-    icon: {
-      active: palette.darkIcons.active,
-      inactive: palette.darkIcons.inactive,
-    },
+  dark: true,
+  primary: palette.teal.base,
+  secondary: palette.orange.base,
+  normal: "#272727",
+  bg: {
+    primary: palette.grey.darken4,
+    secondary: palette.grey.darken3,
+    disabled: palette.grey.darken2,
   },
-
-  fontSizes: [12, 14, 16, 20, 24, 32],
+  text: {
+    primary: palette.lightText.primary,
+    secondary: palette.lightText.secondary,
+    disabled: palette.lightText.disabled,
+    dividers: palette.lightText.dividers,
+  },
+  icon: {
+    active: palette.darkIcons.active,
+    inactive: palette.darkIcons.inactive,
+  },
 };
