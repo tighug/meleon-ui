3dc1ee677029e1c79acbe7f5897575e90e259c69
tighug <kapuriko123@gmail.com>
2020-08-22 12:07:22 +0900

    feat: Update standard.ts

--- src/themes/standard.ts
+++ src/themes/standard.ts
@@ -1,23 +1,25 @@
 import * as palette from "meleon-palette";
+import { Theme } from "./Theme";
 
-export default {
-  dark: true,
-  primary: palette.teal.base,
-  secondary: palette.orange.base,
+const standard: Theme = {
   normal: "#272727",
+  primary: palette.teal.base,
+  secondary: palette.deepOrange.base,
+  error: "#FF5252",
+  info: "#2196F3",
+  success: "#4CAF50",
+  warning: "#FFC107",
   bg: {
-    primary: palette.grey.darken4,
-    secondary: palette.grey.darken3,
-    disabled: palette.grey.darken2,
+    primary: "#242424",
+    secondary: "#303030",
+    disabled: "#242424",
   },
   text: {
-    primary: palette.lightText.primary,
-    secondary: palette.lightText.secondary,
-    disabled: palette.lightText.disabled,
-    dividers: palette.lightText.dividers,
-  },
-  icon: {
-    active: palette.darkIcons.active,
-    inactive: palette.darkIcons.inactive,
+    primary: "rgba(255, 255, 255, 1)",
+    secondary: "rgba(255, 255, 255, 0.7)",
+    disabled: "rgba(255, 255, 255, 0.3)",
+    dividers: "rgba(255, 255, 255, 0.12)",
   },
 };
+
+export default standard;
