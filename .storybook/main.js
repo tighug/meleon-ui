1ca883ab3b254e35f31f18d2b7800727fc8f18d0
tighug <kapuriko123@gmail.com>
2021-05-16 17:14:55 +0900

    chore: Configure storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,16 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ["../src/**/*.stories.tsx"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-postcss",
+    "storybook-dark-mode",
+  ],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ],
+  }),
 };
