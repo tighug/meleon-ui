353abce8e17da35896c73733f8c28b112c663021
tighug <tighug.4696@gmail.com>
2021-06-13 23:04:49 +0900

    chore: Update configs for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [
@@ -13,4 +15,14 @@ module.exports = {
       ["@babel/plugin-proposal-private-methods", { loose: true }],
     ],
   }),
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias = {
+      "@components": path.resolve(__dirname, "../src", "components"),
+      "@themes": path.resolve(__dirname, "../src", "themes"),
+      "@systems": path.resolve(__dirname, "../src", "systems"),
+      "@utils": path.resolve(__dirname, "../src", "utils"),
+    };
+
+    return config;
+  },
 };
